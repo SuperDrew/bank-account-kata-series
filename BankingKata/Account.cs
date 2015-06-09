e54f06a8d79b12e19e892ca826dc1af11b283f0f
SuperDrew <andrew.fraser@red-gate.com>
2015-06-09 17:26:51 +0100

    Green: New account has zero cash.

--- BankingKata/Account.cs
+++ BankingKata/Account.cs
@@ -4,6 +4,8 @@ namespace BankingKata
 {
     public class Account
     {
+        private readonly Cash _cash = new Cash(0.0);
+
         public Account()
         {
         }
@@ -14,10 +16,30 @@ namespace BankingKata
             {
                 throw new ArgumentNullException("Cash can't be null.");
             }
+
+            _cash = cash;
         }
 
         public void Deposit(Cash cash)
         {
+        }
+
+        protected bool Equals(Account other)
+        {
+            return _cash.Equals(other._cash);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Account) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return _cash.GetHashCode();
         }
     }
 }
