15e1b696446536362a741e5284ca7db7a87fc761
SuperDrew <andrew.fraser@red-gate.com>
2015-06-12 17:29:46 +0100

    Added CashMaths feature and steps.

---
+++ BankingKataSpecs/CashSpecs/CashMathSteps.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BankingKata;
+using BankingKataSpecs.AccountSpecs;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace BankingKataSpecs.CashSpecs
+{
+    [Binding]
+    public sealed class CashMathSteps
+    {
+        private AccountData accountData;
+
+        public CashMathSteps(AccountData accountData)
+        {
+            this.accountData = accountData;
+        }
+
+        [Given(@"I have (.*) cash")]
+        public void GivenIhaveCash(int p0)
+        {
+            accountData.Cash = new Cash(1.0);
+        }
+
+        [When(@"I add (.*) cash")]
+        public void WhenIAddCash(int p0)
+        {
+            accountData.Cash += new Cash(p0);
+        }
+        
+        [When(@"I subtract (.*) cash")]
+        public void WhenISubtractCash(int p0)
+        {
+            accountData.Cash -= new Cash(p0);
+        }
+
+        [Then(@"I should have (.*) cash")]
+        public void ThenIShouldHaveCash(int p0)
+        {
+            //Arrange
+            var expectedCash = new Cash(p0);
+
+            //Assert
+            Assert.That(accountData.Cash, Is.EqualTo(expectedCash));
+        }
+
+
+    }
+}
---
+++ BankingKataSpecs/CashSpecs/CashMaths.feature
@@ -0,0 +1,15 @@
+﻿Feature: CashMaths
+	In order to change the amount of cash
+	As a math idiot
+	I want to be able to performmaths on my cash
+
+@mytag
+Scenario: Add cash
+	Given I have 1 cash
+	When I add 1 cash
+	Then I should have 2 cash
+
+Scenario: Subtract cash
+	Given I have 1 cash
+	When I subtract 1 cash
+	Then I should have 0 cash
