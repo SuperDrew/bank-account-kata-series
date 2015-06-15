2318cc07064cb6fa4caf448696056052102ac1ba
SuperDrew <andrew.fraser@red-gate.com>
2015-06-15 16:39:17 +0100

    Attempt to add nSpec project - so far tests aren't working.

---
+++ BankingKataNSpec/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankingKata;
+using NSpec;
+
+namespace BankingKataNSpec
+{
+    public class Test : nspec
+    {
+        private void foo()
+        {
+            Account account = new Account();
+            account.should_be_null();
+            account.should_not_be_null();
+        }
+    }
+}
