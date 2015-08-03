f7e996c9da0621861e97b437e374123cce6450c8
Michael Clark <michael.clark@red-gate.com>
2015-08-03 15:55:09 +0100

    Re-use debit for cheque.

--- BankingKata/Cheque.cs
+++ BankingKata/Cheque.cs
@@ -2,23 +2,19 @@
 
 namespace BankingKata
 {
-    public class Cheque : ATMDebitEntry
+    public class Cheque : DebitEntry
     {
         private readonly int _chequeNumber;
-        private readonly Money _amount;
-        private readonly DateTime _transactionDate;
 
         public Cheque(DateTime transactionDate, Money amount, int chequeNumber)
             : base(transactionDate, amount)
         {
-            _transactionDate = transactionDate;
             _chequeNumber = chequeNumber;
-            _amount = amount;
         }
         
         public override string ToString()
         {
-            return string.Format("CHQ {0} {1} ({2})", _chequeNumber, _transactionDate.ToString("dd MMM yyyy"), _amount);
+            return string.Format("CHQ {0} {1}", _chequeNumber, base.ToString());
         }
     }
 }
