07849ce73cad920207be24dd44b6fa06f6362d15
Michael Clark <michael.clark@red-gate.com>
2015-08-03 15:47:40 +0100

    Re-use withdrawn code from Debit in Cheque

--- BankingKata/Cheque.cs
+++ BankingKata/Cheque.cs
@@ -2,24 +2,20 @@
 
 namespace BankingKata
 {
-    public class Cheque : ITransaction
+    public class Cheque : DebitEntry
     {
         private readonly int _chequeNumber;
         private readonly Money _amount;
         private readonly DateTime _transactionDate;
 
         public Cheque(DateTime transactionDate, Money amount, int chequeNumber)
+            : base(transactionDate, amount)
         {
             _transactionDate = transactionDate;
             _chequeNumber = chequeNumber;
             _amount = amount;
         }
-
-        public Money ApplyTo(Money balance)
-        {
-            return balance - _amount;
-        }
-
+        
         public override string ToString()
         {
             return string.Format("CHQ {0} {1} ({2})", _chequeNumber, _transactionDate.ToString("dd MMM yyyy"), _amount);
